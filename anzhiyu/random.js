46428bbb418b219259ac407576d6618a7dac204c
zhi-an <672596089@qq.com>
2023-10-12 11:18:28 +0800

    Site updated: 2023-10-12 11:18:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/undefined.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
